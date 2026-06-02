6c54c7c7a61ca783ce178b7565f867982b91f027
Aleksandra Iakimova <yakimova.cg@gmail.com>
2026-06-02 20:42:54 +0200

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        Map <String, List<String>> result = new HashMap<>();
+
+        for (String word: strs) {
+        
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            if(!result.containsKey(key)) {
+                result.put(key, new ArrayList<>()); 
+            } 
+            result.get(key).add(word);
+        } 
+
+        return new ArrayList<>(result.values());
+    }
+}
