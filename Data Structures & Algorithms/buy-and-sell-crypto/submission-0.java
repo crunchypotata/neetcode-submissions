3f99d584dfffe3627e6a9932e15c86cc84166801
Aleksandra Iakimova <yakimova.cg@gmail.com>
2026-06-10 17:51:20 +0200

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int left = 0;
+        int right = 1;
+        int profit = 0;
+
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                int currentProfit = prices[right] - prices[left];
+                
+                profit = Math.max(profit, currentProfit); 
+                
+            } else {
+                left = right;
+                
+            }
+            right++;
+        }
+
+        return profit;
+    }
+}
