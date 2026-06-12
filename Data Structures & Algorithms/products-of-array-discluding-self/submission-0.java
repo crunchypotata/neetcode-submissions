7e1fee74ef777c9db87f969f91043fb7c6850449
Aleksandra Iakimova <yakimova.cg@gmail.com>
2026-06-13 00:57:51 +0200

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        int[] result = new int[nums.length];
+        int prefix = 1;
+        int postfix = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = prefix; // Записываем накопленный префикс в ячейку i
+            prefix = prefix * nums[i];
+        }
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            // 2. Умножаем уже лежащий там префикс на накопленный постфикс
+            result[i] = result[i] * postfix; 
+    
+            // 3. Обновляем постфикс для следующего (левого) шага
+            postfix = postfix * nums[i]; 
+        }
+
+        return result;
+    }
+}  
